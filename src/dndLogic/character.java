10645f8ccb74566972fd1ccc85d5b1eab049502c
Greg <gregorykeeton@gmail.com>
2018-07-08 17:33:26 -0500

    added character class and modified generator main for testing purposes

---
+++ src/dndLogic/character.java
@@ -0,0 +1,105 @@
+package dndLogic;
+
+public class character {
+    String name;
+    String race;
+    boolean gender;
+    int specialization;
+    int speed;
+
+    // Ability scores
+    int strength;
+    int dexterity;
+    int constitution;
+    int intelligence;
+    int wisdom;
+    int charisma;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public int getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(int specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
+}
--- src/generator.java
+++ src/generator.java
@@ -1,10 +1,27 @@
 import dndLogic.attribute;
+import dndLogic.character;
 
 public class generator {
 
     public static void main(String[] args) {
-        attribute strength = new attribute();
-        System.out.println(strength.rollD6());
-        System.out.println(strength.rollAbilityScore());
+        attribute att = new attribute();
+        character pc = new character();
+
+        pc.setStrength(att.rollAbilityScore());
+        pc.setDexterity(att.rollAbilityScore());
+        pc.setConstitution(att.rollAbilityScore());
+        pc.setIntelligence(att.rollAbilityScore());
+        pc.setWisdom(att.rollAbilityScore());
+        pc.setCharisma(att.rollAbilityScore());
+
+        System.out.println("Strength:\t" + pc.getStrength());
+        System.out.println("Dexterity:\t" + pc.getDexterity());
+        System.out.println("Constitution:\t" + pc.getConstitution());
+        System.out.println("Intelligence:\t" + pc.getIntelligence());
+        System.out.println("Wisdom:\t" + pc.getWisdom());
+        System.out.println("Charisma:\t" + pc. getCharisma());
+
+        //System.out.println(strength.rollD6());
+        //System.out.println(strength.rollAbilityScore());
     }
 }
