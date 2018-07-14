442f75b672ae751da0766bbf445ea0f206e800fe
Greg <gregorykeeton@gmail.com>
2018-07-13 23:34:43 -0500

    added function for allocating ability scores and modified generator for testing

--- src/dndLogic/character.java
+++ src/dndLogic/character.java
@@ -1,5 +1,23 @@
 package dndLogic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ *  barbarian - 0
+ *  bard      - 1
+ *  cleric    - 2
+ *  druid     - 3
+ *  fighter   - 4
+ *  monk      - 5
+ *  paladin   - 6
+ *  ranger    - 7
+ *  rogue     - 8
+ *  sorcerer  - 9
+ *  warlock   - 10
+ *  wizard    - 11
+ */
 public class character {
     String name;
     String race;
@@ -101,5 +119,61 @@ public class character {
 
     public void setCharisma(int charisma) {
         this.charisma = charisma;
+    }
+
+    public void allocateScores(int scores[]) throws IOException {
+        if(scores.length == 6) {
+            if(this.specialization == 0) {
+                this.setIntelligence(scores[0]);
+                this.setCharisma(scores[1]);
+                this.setWisdom(scores[2]);
+                this.setDexterity(scores[3]);
+                this.setConstitution(scores[4]);
+                this.setStrength(scores[5]);
+            }
+            else if(this.specialization == 1) {
+                this.setIntelligence(scores[0]);
+                this.setWisdom(scores[1]);
+                this.setStrength(scores[2]);
+                this.setConstitution(scores[3]);
+                this.setDexterity(scores[4]);
+                this.setCharisma(scores[5]);
+            }
+            //else if(this.specialization == 2) {
+
+            //}
+            else if(this.specialization == 3) {
+                this.setStrength(scores[0]);
+                this.setCharisma(scores[1]);
+                this.setIntelligence(scores[2]);
+                this.setConstitution(scores[3]);
+                this.setDexterity(scores[4]);
+                this.setWisdom(scores[5]);
+            }
+            else if(this.specialization == 4) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                int choice;
+                System.out.print("Would you prefer Dexterity (0) or Strength (1) as your main ability score?\t");
+                choice = Integer.parseInt(reader.readLine());
+
+                if(choice == 1) {
+                    this.setDexterity(scores[0]);
+                    this.setIntelligence(scores[1]);
+                    this.setCharisma(scores[2]);
+                    this.setWisdom(scores[3]);
+                    this.setConstitution(scores[4]);
+                    this.setStrength(scores[5]);
+                }
+                else if(choice == 0) {
+                    this.setStrength(scores[0]);
+                    this.setIntelligence(scores[1]);
+                    this.setCharisma(scores[2]);
+                    this.setWisdom(scores[3]);
+                    this.setConstitution(scores[4]);
+                    this.setDexterity(scores[5]);
+                }
+
+            }
+        }
     }
 }
--- src/generator.java
+++ src/generator.java
@@ -1,27 +1,52 @@
 import dndLogic.attribute;
 import dndLogic.character;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class generator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         attribute att = new attribute();
         character pc = new character();
+        int scores[] = new int[6];
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        /*
         pc.setStrength(att.rollAbilityScore());
         pc.setDexterity(att.rollAbilityScore());
         pc.setConstitution(att.rollAbilityScore());
         pc.setIntelligence(att.rollAbilityScore());
         pc.setWisdom(att.rollAbilityScore());
         pc.setCharisma(att.rollAbilityScore());
+        */
 
-        System.out.println("Strength:\t" + pc.getStrength());
+        for(int i = 0; i < 6; i++) {
+            scores[i] = att.rollAbilityScore();
+        }
+
+        Arrays.sort(scores);
+
+        System.out.println("0. Barbarian");
+        System.out.println("1. Bard");
+        System.out.println("2. Cleric");
+        System.out.println("3. Druid");
+        System.out.println("4. Fighter");
+
+        // User selects the class they want to build
+        System.out.print("Please type the number of the class you wish to pick:\t");
+        pc.setSpecialization(Integer.parseInt(reader.readLine()));
+
+        // Invoke function to apply most efficient ability score allocation
+        pc.allocateScores(scores);
+
+        System.out.println("\n\nStrength:\t" + pc.getStrength());
         System.out.println("Dexterity:\t" + pc.getDexterity());
         System.out.println("Constitution:\t" + pc.getConstitution());
         System.out.println("Intelligence:\t" + pc.getIntelligence());
         System.out.println("Wisdom:\t" + pc.getWisdom());
         System.out.println("Charisma:\t" + pc. getCharisma());
-
-        //System.out.println(strength.rollD6());
-        //System.out.println(strength.rollAbilityScore());
     }
 }
