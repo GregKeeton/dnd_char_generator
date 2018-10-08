fdde443f9193da048771abaf3531925626cad1df
Greg <gregorykeeton@gmail.com>
2018-10-08 00:52:04 -0500

    Added methods and menu options for ability score increases based on user's desired race

--- src/dndLogic/character.java
+++ src/dndLogic/character.java
@@ -19,9 +19,24 @@ import java.io.InputStreamReader;
  *  wizard    - 11
  */
 public class character {
+
+    // Constant declaring request to choose subrace
+    public static final String SUBRACE = "Choose a subrace:\t";
+
+    // Constant declaring request to pick two other ability scores to increase
+    // For Half Elf
+    public static final String ABIL_INC_HALF_ELF = ", you may pick two extra" +
+            " ability scores to increase by +1\n0. Strength\n1. Dexterity" +
+            "\n2. Constitution\n3. Intelligence\n4. Wisdom\n5. Charisma\n";
+
+    // Constant declaring request to pick one other ability score to increase
+    // for Tiefling
+    public static final String ABIL_INC_TIEFLING = ", you may pick one extra" +
+            " ability score to increase by +2\n0. Charisma\n1. Dexterity\n";
+
     private String name;
     private String race;
-    private boolean gender;
+    private String gender;
     private int specialization;
     private int speed;
 
@@ -33,10 +48,34 @@ public class character {
     private int wisdom;
     private int charisma;
 
+    // Specializations
+    private String[] specs = {"Barbarian", "Bard", "Cleric",
+                              "Druid", "Fighter", "Monk",
+                              "Paladin", "Ranger", "Rogue",
+                              "Sorcerer", "Warlock", "Wizard"};
+
     // Races
     private String[] races = {"Dragonborn", "Dwarf", "Elf",
                               "Gnome", "Half Elf", "Half Orc",
                               "Halfling", "Human", "Tiefling"};
+
+    public String toString() {
+        return "Your character is " + this.getName() + ", a " +
+                this.getGender() + " " + this.getRace() + ".  " +
+                this.getName() + " is a " +
+                this.getSpecialization().toLowerCase() +
+                " by trade.  Their ability scores are:" +
+                this.displayAbilityScores();
+    }
+
+    public String displayAbilityScores() {
+        return "\n\nStrength:\t" + this.getStrength() +
+        "\nDexterity:\t" + this.getDexterity() +
+        "\nConstitution:\t" + this.getConstitution() +
+        "\nIntelligence:\t" + this.getIntelligence() +
+        "\nWisdom:\t" + this.getWisdom() +
+        "\nCharisma:\t" + this. getCharisma();
+    }
 
     public String getName() {
         return name;
@@ -54,16 +93,19 @@ public class character {
         this.race = races[race];
     }
 
-    public boolean isGender() {
+    public String getGender() {
         return gender;
     }
 
-    public void setGender(boolean gender) {
-        this.gender = gender;
+    public void setGender(int gender) {
+        if(gender == 0)
+            this.gender = "male";
+        else if(gender == 1)
+            this.gender = "female";
     }
 
-    public int getSpecialization() {
-        return specialization;
+    public String getSpecialization() {
+        return this.specs[this.specialization];
     }
 
     public void setSpecialization(int specialization) {
@@ -257,6 +299,209 @@ public class character {
                 this.setConstitution(scores[4]);
                 this.setIntelligence(scores[5]);
             }
+        }
+    }
+
+    private void incByOne(int choice) {
+        if(choice == 0)
+            this.strength++;
+        else if(choice == 1)
+            this.dexterity++;
+        else if(choice == 2)
+            this.constitution++;
+        else if(choice == 3)
+            this.intelligence++;
+        else if(choice == 4)
+            this.wisdom++;
+        else if(choice == 5)
+            this.charisma++;
+    }
+
+    public void addRacialModifiers() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int choice;
+
+        // Dragonborn
+        if(this.race.equals(this.races[0])) {
+            this.strength += 2;
+            this.charisma++;
+        }
+        // Dwarf
+        else if(this.race.equals(this.races[1])) {
+            // All dwarves get +2 to constitution by default
+            this.constitution += 2;
+
+            // Prompt user to pick subrace
+            System.out.println(SUBRACE);
+            System.out.print("0. Hill Dwarf\n1. Mountain Dwarf\n");
+            choice = Integer.parseInt(reader.readLine());
+
+            // Input validation
+            while(choice < 0 || choice > 1) {
+                System.out.println("\nInvalid choice entered.  Please enter a number from 0 - 11:\t");
+                choice = Integer.parseInt(reader.readLine());
+            }
+
+            // Hill Dwarf gets wisdom +1 and hitpoint max +1
+            if(choice == 0) {
+                this.wisdom++;
+                // need to implement hp system
+            }
+            // Mountain Dwarf gets strength +2
+            else if(choice == 1) {
+                this.strength += 2;
+            }
+        }
+        // Elf
+        else if(this.race.equals(this.races[2])) {
+            // All elves get +2 to dexterity by default
+            this.dexterity += 2;
+
+            // Prompt user to pick subrace
+            System.out.println(SUBRACE);
+            System.out.print("0. High Elf\n1. Wood Elf\n2. Dark Elf (Drow)\n");
+            choice = Integer.parseInt(reader.readLine());
+
+            // Input validation
+            while(choice < 0 || choice > 2) {
+                System.out.println("\nInvalid choice entered.  Please enter a number from 0 - 11:\t");
+                choice = Integer.parseInt(reader.readLine());
+            }
+
+            // High Elf gets intelligence +1
+            if(choice == 0) {
+                this.intelligence++;
+            }
+            // Wood Elf gets wisdom +1
+            else if(choice == 1) {
+                this.wisdom++;
+            }
+            // Dark Elf gets charisma +1
+            else if(choice == 2) {
+                this.charisma++;
+            }
+        }
+        // Gnome
+        else if(this.race.equals(this.races[3])) {
+            // All gnomes get +2 to intelligence and 25 speed by default
+            this.intelligence += 2;
+            // Set speed
+            this.setSpeed(25);
+
+            // Prompt user to pick subrace
+            System.out.println(SUBRACE);
+            System.out.print("0. Forest Gnome\n1. Rock Gnome\n2. Deep Gnome\n");
+            choice = Integer.parseInt(reader.readLine());
+
+            // Input validation
+            while(choice < 0 || choice > 2) {
+                System.out.println("\nInvalid choice entered.  Please enter a number from 0 - 11:\t");
+                choice = Integer.parseInt(reader.readLine());
+            }
+
+            // Forest Gnome gets dexterity +1
+            if(choice == 0) {
+                this.dexterity++;
+            }
+            // Rock Gnome gets constitution +1
+            else if(choice == 1) {
+                this.constitution++;
+            }
+            // Deep Gnome gets intelligence +2 and dexterity +1
+            else if(choice == 2) {
+                this.intelligence += 2;
+                this.dexterity++;
+            }
+        }
+        // Half Elf
+        else if(this.race.equals(this.races[4])) {
+            // All half elves get +2 to charisma and +1 to two other ability
+            // scores of their choice and speed of 30 by default
+            this.charisma += 2;
+
+            System.out.print("\n\nAs a " + this.race + ABIL_INC_HALF_ELF);
+            choice = Integer.parseInt(reader.readLine());
+
+            // Input validation
+            while(choice < 0 || choice > 2) {
+                System.out.println("\nInvalid choice entered.  Please enter a number from 0 - 11:\t");
+                choice = Integer.parseInt(reader.readLine());
+            }
+
+            // First choice increased
+            this.incByOne(choice);
+
+            choice = Integer.parseInt(reader.readLine());
+
+            // Second choice increased
+            this.incByOne(choice);
+
+            // Set speed
+            this.setSpeed(30);
+        }
+        // Half Orc
+        else if(this.race.equals(this.races[5])) {
+            // All half orcs get +2 to strength and +1 to constitution
+            // as well as 30 speed by default
+            this.strength += 2;
+            this.constitution++;
+            // Set speed
+            this.setSpeed(30);
+        }
+        // Halfling
+        else if(this.race.equals(this.races[6])) {
+            // All halflings get +2 to dexterity as well as 25 speed by default
+            this.dexterity += 2;
+            // Set speed
+            this.setSpeed(25);
+
+            // Prompt user to pick subrace
+            System.out.println(SUBRACE);
+            System.out.print("0. Lightfoot\n1. Stout\n");
+            choice = Integer.parseInt(reader.readLine());
+
+            // Input validation
+            while(choice < 0 || choice > 5) {
+                System.out.println("\nInvalid choice entered.  Please enter a number from 0 - 11:\t");
+                choice = Integer.parseInt(reader.readLine());
+            }
+
+            // Lightfoot gets charisma +1
+            if(choice == 0)
+                this.charisma++;
+            // Sout gets constitution +1
+            else if(choice == 1)
+                this.constitution++;
+        }
+        // Human
+        else if(this.race.equals(this.races[7])) {
+            // All humans get +1 to EACH of their ability scores and 30 speed
+            // by default
+            for(int i = 0; i < 6; i++) {
+                this.incByOne(i);
+            }
+            // Set speed
+            this.setSpeed(30);
+        }
+        // Tiefling
+        else if(this.race.equals(this.races[8])) {
+            // All tieflings get +1 to intelligence and +2 to their choice of
+            // either dexterity or charisma as well as 30 speed by default
+            this.intelligence++;
+            System.out.println("\n\nAs a " + this.race + ABIL_INC_TIEFLING);
+            choice = Integer.parseInt(reader.readLine());
+
+            // Input validation
+            while(choice < 0 || choice > 1) {
+                System.out.println("\nInvalid choice entered.  Please enter a number from 0 - 11:\t");
+                choice = Integer.parseInt(reader.readLine());
+            }
+
+            // Charisma increase
+            if(choice == 0)
+                this.charisma += 2;
+            else if(choice == 1)
+                this.dexterity += 2;
         }
     }
 }
--- src/generator.java
+++ src/generator.java
@@ -11,52 +11,70 @@ public class generator {
     public static final String CLASS_SELECT = "0. Barbarian\n" +
             "1. Bard\n2. Cleric\n3. Druid\n4. Fighter\n5. Monk\n" +
             "6. Paladin\n7. Ranger\n8. Rogue\n9. Sorcerer\n" +
-            "10. Warlock\n11. Wizard\n";
+            "10. Warlock\n11. Wizard\n\n" +
+            "Please type the number of the class you wish to pick:\t";
 
     // Constant declaring race selection menu
     public static final String RACE_SELECT = "\n\n0. Dragonborn\n" +
             "1. Dwarf\n2. Elf\n3. Gnome\n4. Half Elf\n5. Half Orc\n" +
-            "6. Halfling\n7. Human\n8. Tiefling\n";
+            "6. Halfling\n7. Human\n8. Tiefling\n\n" +
+            "Select the race you would like to play as:\t";
+
+    // Constant declaring gender selection menu
+    public static final String GENDER_SELECT = "\n\n0. Male\n1. Female\n\n" +
+            "Select the gender you would like for your character:\t";
 
     public static void main(String[] args) throws IOException {
         attribute att = new attribute();
         character pc = new character();
         int scores[] = new int[6];
 
+        // Loop control
+        boolean choice = true;
+
+        // For getting user input
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        // Generate ability scores and store in an array for access
-        for(int i = 0; i < 6; i++) {
-            scores[i] = att.rollAbilityScore();
+        while (choice) {
+            // Generate ability scores and store in an array for access
+            for (int i = 0; i < 6; i++) {
+                scores[i] = att.rollAbilityScore();
+            }
+
+            // Sort the scores so that we know the order from smallest to greatest
+            Arrays.sort(scores);
+
+            // Display the class selection menu to the user
+            System.out.println(CLASS_SELECT);
+            pc.setSpecialization(Integer.parseInt(reader.readLine()));
+
+            // Invoke function to apply most efficient ability score allocation
+            pc.allocateScores(scores);
+
+            // Display the race selection menu to the user
+            System.out.println(RACE_SELECT);
+            pc.setRace(Integer.parseInt(reader.readLine()));
+
+            // Get user's choices for racial modifiers
+            pc.addRacialModifiers();
+
+            // Display the gender selection menu to the user
+            System.out.println(GENDER_SELECT);
+            pc.setGender(Integer.parseInt(reader.readLine()));
+
+            // Get the character's name from the user
+            System.out.print("\n\nWhat is the character's name?");
+            pc.setName(reader.readLine());
+
+            System.out.print(pc.toString());
+
+            // Continue making characters?
+            System.out.println("\n\n\nWould you like to roll up another character?  (y/n)");
+            String cont = reader.readLine();
+
+            // Change loop control if user wishes to stop
+            if(cont.toLowerCase().charAt(0) == 'n')
+                choice = false;
         }
-
-        // Sort the scores so that we know the order from smallest to greatest
-        Arrays.sort(scores);
-
-        // Display the class selection menu to the user
-        System.out.print(CLASS_SELECT);
-
-        // User selects the class they want to build
-        System.out.print("Please type the number of the class you wish to pick:\t");
-        pc.setSpecialization(Integer.parseInt(reader.readLine()));
-
-        // Invoke function to apply most efficient ability score allocation
-        pc.allocateScores(scores);
-
-        // Display the race selection menu to the user
-        System.out.print(RACE_SELECT);
-
-        // User selects the race they want to play as
-        System.out.print("Select the race you would like to play as:\t");
-        pc.setRace(Integer.parseInt(reader.readLine()));
-
-        System.out.print('\n' + pc.getRace());
-
-        System.out.println("\n\nStrength:\t" + pc.getStrength());
-        System.out.println("Dexterity:\t" + pc.getDexterity());
-        System.out.println("Constitution:\t" + pc.getConstitution());
-        System.out.println("Intelligence:\t" + pc.getIntelligence());
-        System.out.println("Wisdom:\t" + pc.getWisdom());
-        System.out.println("Charisma:\t" + pc. getCharisma());
     }
 }
